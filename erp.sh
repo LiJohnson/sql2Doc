58d9785c5893045e45479051db0a9f253183ca8a
LiJohnson <598420668@163.com>
2024-03-19 18:45:42 +0800

    update

--- erp.sh
+++ erp.sh
@@ -7,7 +7,8 @@ OUT=/tmp/sql_doc
 mkdir $OUT
 rm -rf $OUT/*
 
-sql=$(mysqldump -h192.168.1.109 -uerp -perp -d --compact --databases erp_ht | sed -e 's/^\/\*![0-4][0-9]\{4\}.*\/;$//g')
+# sql=$(mysqldump -h192.168.1.109  -uerp -perp -d --compact --databases erp_ht | sed -e 's/^\/\*![0-4][0-9]\{4\}.*\/;$//g')
+sql=$(ssh erp-255 "docker run --rm  mysql  mysqldump -h192.168.0.197  -uroot -p'76sQa~23' -d --compact --databases erp_ht" | sed -e  's/^\/\*![0-5][0-9]\{4\}.*\/;$//g' | sed 's/) COLLATE utf8mb4_general_ci/)/g' | sed 's/text COLLATE utf8mb4_general_ci/text/g' | sed 's/DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci//g')
 
 echo $sql | node sql2adoc.js $OUT
 echo $sql | node sql2md.js $OUT
@@ -29,5 +30,5 @@ echo $OUT/数据表关系.svg
 echo $OUT/pd.pdm
 echo $OUT/db.sql
 
-scp $OUT/pd.pdm win10.jh.com:/c/Users/lcs/Desktop/erp-db-designer/erp/哈尔滨变压器.pdm
-scp $OUT/sql-doc.html zs-109:/tmp/erp/erp_ht.html
+# scp $OUT/pd.pdm win10.jh.com:/c/Users/lcs/Desktop/erp-db-designer/erp/哈尔滨变压器.pdm
+scp $OUT/sql-doc.html $OUT/数据表关系-1.svg zs-109:/data/service/erp-html/docs/
